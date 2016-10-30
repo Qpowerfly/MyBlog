69d864e967a1fb6901cd5c8a7ec372953c47ded1
XCL <80213876@qq.com>
2016-10-30 18:32:08 +0800

    update

--- MyBlog/MyBlogWeb/Controllers/BaseController.cs
+++ MyBlog/MyBlogWeb/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace MyBlogWeb.Controllers
 {
@@ -16,7 +17,14 @@ namespace MyBlogWeb.Controllers
         {
             get
             {
-                return XCLCMS.Lib.Login.LoginHelper.CreateUserToken(XCLNetTools.XML.ConfigClass.GetConfigString("MainUserName"), XCLNetTools.XML.ConfigClass.GetConfigString("MainPwd"));
+                var request = XCLCMS.Lib.WebAPI.Library.CreateRequest<XCLCMS.Data.Model.Custom.UserNamePwd>(null);
+                request.Body = new XCLCMS.Data.Model.Custom.UserNamePwd()
+                {
+                    UserName = XCLNetTools.XML.ConfigClass.GetConfigString("MainUserName"),
+                    Pwd = XCLNetTools.XML.ConfigClass.GetConfigString("MainPwd")
+                };
+                var response = XCLCMS.Lib.WebAPI.OpenAPI.CreateUserToken(request);
+                return null == response ? string.Empty : Convert.ToString(response.Body);
             }
         }
 
